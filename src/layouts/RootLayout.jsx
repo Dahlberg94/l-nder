dc3dc82f4980bcef0dfd7fefcebd7405ba1988fe
Dahlberg94 <Marcus_dahlberg_94@hotmail.com>
2024-11-10 20:37:19 +0100

    header dune

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -7,14 +7,42 @@ import logoMoon from "../assets/moon.png";
 
 const RootLayout = () => {
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  const [animate, setAnimate] = useState(false);
 
   useEffect(() => {
     localStorage.setItem("theme", theme);
     document.body.className = theme;
+
+    // Trigger animation when theme changes
+    setAnimate(true);
+    const timeoutId = setTimeout(() => {
+      setAnimate(false);
+    }, 1000); // Reset animation state after 1 second
+
+    return () => clearTimeout(timeoutId);
   }, [theme]);
 
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  // Function for ripple effect
+  const handleRipple = (e) => {
+    const button = e.currentTarget;
+
+    const ripple = document.createElement("span");
+    ripple.className = "ripple";
+
+    const rect = button.getBoundingClientRect();
+    const size = Math.max(rect.width, rect.height);
+    ripple.style.width = ripple.style.height = `${size}px`;
+    ripple.style.left = `${e.clientX - rect.left - size / 2}px`;
+    ripple.style.top = `${e.clientY - rect.top - size / 2}px`;
+
+    button.appendChild(ripple);
+    ripple.addEventListener("animationend", () => {
+      ripple.remove();
+    });
   };
 
   return (
@@ -27,13 +55,33 @@ const RootLayout = () => {
             alt="logo"
             className="logo"
           />
-          <button onClick={toggleTheme} className="modeButton">
-            <img
-              src={theme === "light" ? logoSun : logoMoon}
-              alt="logo"
-              className="logo__switch"
-              alt=""
-            />{" "}
+          <button
+            onClick={(e) => {
+              toggleTheme();
+              handleRipple(e);
+            }}
+            className="modeButton"
+          >
+            <div className="logo-container">
+              <div className="logo-switch-container">
+                {/* Light Mode: Sun icon slides in, Moon slides out */}
+                <img
+                  src={logoMoon}
+                  alt="logo"
+                  className={`logo__switch ${
+                    animate ? "slide-out-right" : ""
+                  } ${theme === "dark" ? "hidden" : "slide-in-left"}`}
+                />
+
+                <img
+                  src={logoSun}
+                  alt="logo"
+                  className={`logo__switch ${animate ? "slide-out-left" : ""} ${
+                    theme === "light" ? "hidden" : "slide-in-right"
+                  }`}
+                />
+              </div>
+            </div>
             {theme === "light" ? "LIGHT MODE" : "DARK MODE"}
           </button>
         </nav>
